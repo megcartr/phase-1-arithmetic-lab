6be6baf2d318952d6760b1310d79b82fc108e345
Meghan Carter <msc2010@yahoo.com>
2023-04-02 14:33:33 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+{let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;}
 
-// Write your code here
+{let random = 1
+Math.floor(Math.random()*1)+1;}
+
+{let num3 = 40;
+let num4 = 10;
+let mod = num3/num4;}
+
+let max = (15,16,17,18,18,20);
